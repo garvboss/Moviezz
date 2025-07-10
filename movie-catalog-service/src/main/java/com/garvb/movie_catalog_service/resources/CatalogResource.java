6fe3c1c85e7e593569d7ee0350293ad0e8c8a4b9
garvboss <garvb2286@gmail.com>
2025-07-10 11:23:16 +0530

    2. Added Movie Catalog Service API

---
+++ movie-catalog-service/src/main/java/com/garvb/movie_catalog_service/resources/CatalogResource.java
@@ -0,0 +1,23 @@
+package com.garvb.movie_catalog_service.resources;
+
+import com.garvb.movie_catalog_service.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Test Movie", "Test Description", 9)
+        );
+    }
+}

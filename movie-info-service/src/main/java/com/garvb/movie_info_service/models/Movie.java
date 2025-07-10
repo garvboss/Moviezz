e48a76e09e3ce16443b6951ae38ec531e373a9cf
garvboss <garvb2286@gmail.com>
2025-07-10 11:41:53 +0530

    3. Added Movie Info Service API

---
+++ movie-info-service/src/main/java/com/garvb/movie_info_service/models/Movie.java
@@ -0,0 +1,27 @@
+package com.garvb.movie_info_service.models;
+
+public class Movie {
+    String movieId;
+    String name;
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
